52efd1c7ac24cb1f3b4fbe22eaefaa060eabf45e
dnandha <daljeet@gmx.net>
2022-06-04 14:25:32 +0200

    add conditioning

--- preprocess.py
+++ preprocess.py
@@ -3,40 +3,57 @@ import json
 from glob import glob
 from os import path as osp
 
-parser = argparse.ArgumentParser()
-parser.add_argument("dataset_path")
-parser.add_argument("outfile")
-args = parser.parse_args()
+from prune_graph import read_graph, prune_graph
 
 
 class Result(object):
-    def __init__(self, goal, task, scene, instructions, actions):
-        self.goal = goal
-        self.task = task
-        self.scene = scene
-        self.instructions = instructions
-        self.actions = actions
+    def __init__(self, goal, task, scene, target, instructions, actions):
+        self.goal = goal.strip()
+        self.task = task.strip()
+        self.scene = int(scene)
+        self.target = target
+        self.instructions = [i.strip() for i in instructions]
+        self.actions = [a.strip() for a in actions]
 
-    def to_json(self, condition=False):
+    def __len__(self):
+        if self.actions is None:
+            return 0
+        return len(self.actions)
+
+    def gen_action_sequence(self, imax=0):
+        subgoals = []
+
+        if not self.goal.endswith("."):
+            act_seq = self.goal + ":\n"
+        else:
+            act_seq = self.goal.replace(".", ":\n")
+
+        for i, (instr, act) in enumerate(zip(self.instructions, self.actions)):
+            if imax != 0 and i == imax:
+                break
+            subgoals += [{
+                'instruction': instr,
+                'action': act}]
+            act_seq += f"{i}.{act}\n"
+        act_seq += '|'
+        return subgoals, act_seq
+
+    def to_json(self, condition=None, imax=0):
         task = {
             'goal': self.goal,
             'type': self.task,
             'scene': self.scene,
+            'target': self.target,
             'subgoals': [],
-            'instructions': ""
+            'instructions': ""  # TODO: rename
         }
 
         if self.actions is not None:
-            task['instructions'] = ""
-            if condition:
-                task['instructions'] += f"{self.scene}>"
-            task['instructions'] += self.goal.replace(".", ":\n")
-            for i, (instr, act) in enumerate(zip(self.instructions, self.actions)):
-                task['subgoals'] += [{
-                    'instruction': instr,
-                    'action': act}]
-                task['instructions'] += f"{i}.{act}\n"
-            task['instructions'] += '|'
+            task['subgoals'], act_seq = self.gen_action_sequence(imax=imax)
+            if condition is not None:
+                task['instructions'] = condition.get_knowledge(self).lower() + "\n" + act_seq
+            else:
+                task['instructions'] = act_seq
         else:
             task['subgoals'] = self.instructions
 
@@ -44,7 +61,6 @@ class Result(object):
 
 
 def parse_file(file_):
-
     data = json.load(file_)
 
     plan = None
@@ -58,6 +74,7 @@ def parse_file(file_):
         ttype = data['task_type']
 
     scene = data['scene']['scene_num']
+    target = data['pddl_params']['object_target']
 
     acts = None
     if plan:
@@ -79,22 +96,55 @@ def parse_file(file_):
 
         #for act, instr in zip(acts, instrs):
             #print(act, instr)
-        yield Result(goal, ttype, scene, instrs, acts)
+        yield Result(goal, ttype, scene, target, instrs, acts)
 
 
-max_samples = 100000
-i = 0
+class Domain(object):
+    """
+    alfred/gen/constants.py
+    """
+    def __init__(self, file):
+        self.G = read_graph(file)
 
-dpath = osp.join(args.dataset_path, "*/*/traj_data.json")
+    @staticmethod
+    def get_room(id_):
+        if id_ in range(1, 31):
+            return "Kitchen"
+        elif id_ in range(201, 231):
+            return "LivingRoom"
+        elif id_ in range(301, 331):
+            return "Bedroom"
+        elif id_ in range(401, 431):
+            return "Bathroom"
 
-tasks = []
-for path in glob(dpath):
-    i += 1
-    if i >= max_samples:
-        break
-    with open(path) as f:
-        for res in parse_file(f):
-            tasks += [res.to_json(condition=True)]
+    def get_knowledge(self, res):
+        # source (room) and target (object) can later be infered from image and goal
+        source = Domain.get_room(res.scene)
+        target = res.target
+        return prune_graph(self.G, source, target)
 
-with open(args.outfile, 'w') as outfile:
-    json.dump(tasks, outfile)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset_path")
+    parser.add_argument("outfile")
+    parser.add_argument("--cond", help="path to graph")
+    #parser.add_argument("--aug", type=int, help="no. of augmentation steps", default=0)
+    args = parser.parse_args()
+    print(args)
+
+    domain = None
+    if args.cond:
+        domain = Domain(args.cond)
+
+    dpath = osp.join(args.dataset_path, "*/*/traj_data.json")
+
+    tasks = []
+    for path in glob(dpath):
+        with open(path) as f:
+            for res in parse_file(f):
+                #for aug in range(len(res)):
+                tasks += [res.to_json(condition=domain)]
+
+    with open(args.outfile, 'w') as outfile:
+        json.dump(tasks, outfile)
--- prune_graph.py
+++ prune_graph.py
@@ -1,6 +1,17 @@
-import pandas as pd
 import networkx as nx
 from networkx.drawing.nx_pydot import write_dot, graphviz_layout, read_dot
+
+
+def read_graph(file):
+    return read_dot(file)
+
+
+def prune_graph(G, source, target):
+    pred = nx.all_simple_paths(G, source=source, target=target, cutoff=2)
+
+    # drop target and source node
+    pred = ','.join([x[1] for x in pred])
+    return f"{source}-[{pred}]={target}"
 
 
 if __name__ == "__main__":
@@ -12,11 +23,4 @@ if __name__ == "__main__":
     parser.add_argument('target')
     args = parser.parse_args()
 
-    G = read_dot(args.infile)
-    pred = nx.all_simple_paths(G, source=args.source, target=args.target, cutoff=2)
-
-    # drop target and source node
-    print([x for x in pred])
-    pred = ','.join([x for x in pred])
-
-    #print(f"{args.source}-[{pred}]={args.target}")
+    print(prune_graph(read_graph(args.infile), args.source, args.target))
