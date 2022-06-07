3788466bf82c910130cbd6388be2d79781735f03
dnandha <daljeet@gmx.net>
2022-06-07 11:02:41 +0200

    RL technically working

--- roblm.py
+++ roblm.py
@@ -10,7 +10,7 @@ from transformers import AutoModelForSequenceClassification, AutoTokenizer, GPT2
 from transformers import get_scheduler
 from tensorboardX import SummaryWriter
 
-from trl.gpt2 import GPT2HeadWithValueModel
+from trl.gpt2 import GPT2HeadWithValueModel, respond_to_batch
 from trl.ppo import PPOTrainer
 
 from evaluator import Eval
@@ -20,8 +20,8 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--train', help='specify train dataset json')
+    parser.add_argument('--train_rl', help='tune model in RL setting')
     parser.add_argument('--train_epochs', type=int, default=2)
-    parser.add_argument('--train_rl', action='store_true')
     parser.add_argument('--log', help='logfile for tensorboard')
     parser.add_argument('--eval', help='specify valid dataset json')
     parser.add_argument('--eval_topk', type=int, help='use topk sampling')
@@ -56,84 +56,106 @@ if __name__ == "__main__":
         #ds.set_format(type='torch', columns=['input_ids', 'attention_mask', 'attention_mask_aux', 'labels'])
         ds.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
 
-        dl = DataLoader(ds, shuffle=True, batch_size=2 if not args.train_rl else 1)
-
+        dl = DataLoader(ds, shuffle=True, batch_size=2)
         num_training_steps = args.train_epochs * len(dl)
+
+        model.to(device)
+        if os.path.isfile(args.chkpt_path):
+            print("Restoring checkpoint:", args.chkpt_path)
+            model.load_state_dict(torch.load(args.chkpt_path))
+        model.train()
+
+        optimizer = AdamW(model.parameters(), lr=5e-5)
+        lr_scheduler = get_scheduler(
+            name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps
+        )
+
         progress = tqdm(range(num_training_steps))
+        writer = SummaryWriter(args.log)
+        for epoch in range(args.train_epochs):
+            for batch in dl:
+                batch = {k: v.to(device) for k, v in batch.items()}
+                outputs_lm = model(input_ids=batch['input_ids'], attention_mask=batch['attention_mask'], labels=batch['labels'])
 
-        if args.train_rl:
-            model = GPT2HeadWithValueModel.from_pretrained('gpt2')
-            model_ref = GPT2HeadWithValueModel.from_pretrained('gpt2')
+                # simply modify the attention mask such that it focuses on the arguments -> most important for successful plan
+                #outputs_aux = model(input_ids=batch['input_ids'], attention_mask=batch['attention_mask_aux'], labels=batch['labels'])
 
-            model.load_state_dict(torch.load(args.chkpt_path), strict=False)
-            model_ref.load_state_dict(torch.load(args.chkpt_path), strict=False)
+                loss = outputs_lm.loss # + outputs_aux.loss
 
-            ppo_config = {'batch_size': 1, 'forward_batch_size': 1}
-            ppo_trainer = PPOTrainer(model, model_ref, tokenizer, **ppo_config)
-            
-            for epoch in range(args.train_epochs):
-                for batch in dl:
-                    #batch = {k: v.to(device) for k, v in batch.items()}
-                    outputs = model.generate(input_ids=batch['input_ids'])
-                    preds_text = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+                #import pdb; pdb.set_trace()
+                #preds = torch.argmax(outputs.logits, dim=-1)
+                #res = tokenizer.batch_decode(preds, skip_special_tokens=True)[0]
 
-                    labels = batch['labels']
-                    labels_text = tokenizer.batch_decode(labels, skip_special_tokens=True)
+                #if args.auxloss:
+                #    outputs = model.generate(batch['input_ids'], do_sample=False, max_length=200)
+                #    labels_text = tokenizer.batch_decode(batch['labels'], skip_special_tokens=True)
+                #    preds_text = tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
-                    #reward = -torch.sum(torch.tensor([Eval.aux_loss(Eval.proc_instructions(label), Eval.proc_instructions(pred))
-                    #                        for label, pred in zip(preds_text, labels_text)]))
-                    reward = torch.tensor([1])
-                    print(reward)
+                #    for label, pred in zip(labels_text, preds_text):
+                #        aux_loss(proc_instructions(label), proc_instructions(pred))
 
-                    train_stats = ppo_trainer.step(labels, outputs, reward)
-                    print(train_stats)
+                loss.backward()
+                writer.add_scalar('loss/train', loss.item(), progress.n)
 
-                    progress.update(1)
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
+                progress.update(1)
 
-        else:
-            model.to(device)
-            if os.path.isfile(args.chkpt_path):
-                print("Restoring checkpoint:", args.chkpt_path)
-                model.load_state_dict(torch.load(args.chkpt_path))
-            model.train()
+            torch.save(model.state_dict(), args.model_path)
+    elif args.train_rl:
+        def tokenize(e):
+            # queries
+            f = tokenizer(e['instructions'].split(":")[0]+":")  # TODO
+            # responses
+            f['labels'] = tokenizer(e['instructions'].split(":")[1])['input_ids']
+            return f
 
-            optimizer = AdamW(model.parameters(), lr=5e-5)
-            lr_scheduler = get_scheduler(
-                name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps
-            )
+        df = pd.read_json(args.train_rl)
+        ds = Dataset.from_pandas(df)
+        ds = ds.map(tokenize, num_proc=8)
+        ds.set_format(type='torch', columns=['input_ids', 'labels'])
 
-            writer = SummaryWriter(args.log)
-            for epoch in range(args.train_epochs):
-                for batch in dl:
-                    batch = {k: v.to(device) for k, v in batch.items()}
-                    outputs_lm = model(input_ids=batch['input_ids'], attention_mask=batch['attention_mask'], labels=batch['labels'])
+        dl = DataLoader(ds, shuffle=True, batch_size=1)
+        num_training_steps = args.train_epochs * len(dl)
 
-                    # simply modify the attention mask such that it focuses on the arguments -> most important for successful plan
-                    #outputs_aux = model(input_ids=batch['input_ids'], attention_mask=batch['attention_mask_aux'], labels=batch['labels'])
+        model = GPT2HeadWithValueModel.from_pretrained('gpt2')
+        model_ref = GPT2HeadWithValueModel.from_pretrained('gpt2')
 
-                    loss = outputs_lm.loss # + outputs_aux.loss
+        model.to(device)
+        model_ref.to(device)
 
-                    #import pdb; pdb.set_trace()
-                    #preds = torch.argmax(outputs.logits, dim=-1)
-                    #res = tokenizer.batch_decode(preds, skip_special_tokens=True)[0]
+        model.load_state_dict(torch.load(args.chkpt_path), strict=False)
+        model_ref.load_state_dict(torch.load(args.chkpt_path), strict=False)
 
-                    #if args.auxloss:
-                    #    outputs = model.generate(batch['input_ids'], do_sample=False, max_length=200)
-                    #    labels_text = tokenizer.batch_decode(batch['labels'], skip_special_tokens=True)
-                    #    preds_text = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+        ppo_config = {'batch_size': 1, 'forward_batch_size': 1}
+        ppo_trainer = PPOTrainer(model, model_ref, tokenizer, **ppo_config)
+        
+        progress = tqdm(range(num_training_steps))
+        writer = SummaryWriter(args.log)
+        for epoch in range(args.train_epochs):
+            for batch in dl:
+                batch = {k: v.to(device) for k, v in batch.items()}
+                inputs = batch['input_ids']
+                outputs = model.generate(input_ids=inputs, do_sample=False, max_length=200)
+                #outputs = respond_to_batch(model, batch['input_ids'], txt_len=200)
+                preds = outputs[:, inputs.shape[1]:]  # TODO: .generate always includes query.. has to be removed manuall -> use BOS token?
+                preds_text = tokenizer.batch_decode(preds, skip_special_tokens=True)
 
-                    #    for label, pred in zip(labels_text, preds_text):
-                    #        aux_loss(proc_instructions(label), proc_instructions(pred))
+                labels = batch['labels']
+                labels_text = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
-                    loss.backward()
-                    writer.add_scalar('loss/train', loss.item(), progress.n)
+                loss = torch.sum(torch.tensor([Eval.aux_loss(Eval.proc_instructions(label), Eval.proc_instructions(pred))
+                                        for label, pred in zip(preds_text, labels_text)]))
+                rewards = [1. - loss]  # TODO: support other batch size?
 
-                    optimizer.step()
-                    lr_scheduler.step()
-                    optimizer.zero_grad()
-                    progress.update(1)
+                train_stats = ppo_trainer.step(labels, preds, rewards)
+                writer.add_scalar('ppo/model_reward', rewards[0])
+                writer.add_scalar('ppo/return/mean', train_stats['ppo/returns/mean'][0])
+                writer.add_scalar('ppo/return/var', train_stats['ppo/returns/var'][0])
 
-                torch.save(model.state_dict(), args.model_path)
+                progress.update(1)
+            torch.save(model.state_dict(), args.model_path)
     elif args.eval:
         def tokenize(e):
             #f = tokenizer(e['goal'].replace(".",":"))
